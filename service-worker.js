a42ad5615ac78bcccc6aef8521f2210c137c9713
suyoun-1122 <suyoun.son86@gmail.com>
2026-07-18 21:52:15 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-const CACHE="yeonjae-french-v4-3-3";
+const CACHE="yeonjae-french-v4-4-0";
 const CORE=[
   "./","./index.html",
-  "./style.css?v=4.3.3",
-  "./assets/styles/design-system.css?v=4.3.3",
-  "./assets/styles/components.css?v=4.3.3",
-  "./app.js?v=4.3.3","./audio.js","./review.js",
+  "./style.css?v=4.4.0",
+  "./assets/styles/design-system.css?v=4.4.0",
+  "./assets/styles/components.css?v=4.4.0",
+  "./app.js?v=4.4.0","./audio.js","./review.js",
   "./data/words.json?v=4.3.0","./data/lessons.json?v=4.3.0","./data/recipes.json?v=4.3.0",
-  "./assets/characters/petit-clean-v431.png","./assets/characters/fromage-clean-v431.png","./assets/characters/lavande-clean-v431.png","./assets/characters/trio-clean-v431.png","./assets/foods/recipes/croissant.svg","./assets/foods/recipes/baguette.svg","./assets/foods/recipes/crepe.svg","./assets/foods/recipes/madeleine.svg","./assets/foods/recipes/macaron.svg","./assets/foods/recipes/quiche.svg","./assets/foods/recipes/croquemonsieur.svg","./assets/foods/recipes/ratatouille.svg","./assets/foods/recipes/gratin.svg","./assets/foods/recipes/soupe.svg","./assets/foods/recipes/boeuf.svg","./assets/foods/recipes/coq.svg","./assets/foods/recipes/cassoulet.svg","./assets/foods/recipes/bouillabaisse.svg","./assets/foods/recipes/tarte.svg"
+  "./assets/characters/petit-animated.svg","./assets/characters/fromage-animated.svg","./assets/characters/lavande-animated.svg","./assets/characters/trio-animated.svg","./assets/foods/recipes/croissant.svg","./assets/foods/recipes/baguette.svg","./assets/foods/recipes/crepe.svg","./assets/foods/recipes/madeleine.svg","./assets/foods/recipes/macaron.svg","./assets/foods/recipes/quiche.svg","./assets/foods/recipes/croquemonsieur.svg","./assets/foods/recipes/ratatouille.svg","./assets/foods/recipes/gratin.svg","./assets/foods/recipes/soupe.svg","./assets/foods/recipes/boeuf.svg","./assets/foods/recipes/coq.svg","./assets/foods/recipes/cassoulet.svg","./assets/foods/recipes/bouillabaisse.svg","./assets/foods/recipes/tarte.svg"
 ];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)))});
 self.addEventListener("activate",event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
