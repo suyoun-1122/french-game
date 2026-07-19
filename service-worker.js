1c74de12b2793fbfcb520d4ad28a5426344487a7
suyoun-1122 <suyoun.son86@gmail.com>
2026-07-19 17:51:24 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE="yeonjae-french-v4-4-5";
 const CORE=[
   "./","./index.html",
-  "./style.css?v=4.4.5",
-  "./assets/styles/design-system.css?v=4.4.5",
-  "./assets/styles/components.css?v=4.4.5",
-  "./app.js?v=4.4.5","./audio.js","./review.js",
+  "./style.css?v=4.4.6",
+  "./assets/styles/design-system.css?v=4.4.6",
+  "./assets/styles/components.css?v=4.4.6",
+  "./app.js?v=4.4.6","./audio.js","./review.js",
   "./data/words.json?v=4.3.0","./data/lessons.json?v=4.3.0","./data/recipes.json?v=4.3.0",
   "./assets/characters/petit-vector.svg","./assets/characters/fromage-vector.svg","./assets/characters/lavande-vector.svg","./assets/characters/trio-vector.svg",
   "./assets/characters/petit-happy.svg","./assets/characters/petit-think.svg","./assets/characters/fromage-happy.svg","./assets/characters/fromage-think.svg","./assets/characters/lavande-happy.svg","./assets/characters/lavande-think.svg","./assets/foods/recipes/croissant.svg","./assets/foods/recipes/baguette.svg","./assets/foods/recipes/crepe.svg","./assets/foods/recipes/madeleine.svg","./assets/foods/recipes/macaron.svg","./assets/foods/recipes/quiche.svg","./assets/foods/recipes/croquemonsieur.svg","./assets/foods/recipes/ratatouille.svg","./assets/foods/recipes/gratin.svg","./assets/foods/recipes/soupe.svg","./assets/foods/recipes/boeuf.svg","./assets/foods/recipes/coq.svg","./assets/foods/recipes/cassoulet.svg","./assets/foods/recipes/bouillabaisse.svg","./assets/foods/recipes/tarte.svg"
